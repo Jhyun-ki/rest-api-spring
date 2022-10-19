37b21423a7798892e18064d9baab4d3f680aba6a
HyunKi Jung <hjk2334@naver.com>
2022-10-19 16:34:00 +0900

    Event domain

---
+++ src/main/java/com/hyunki/restapi/events/EventStatus.java
@@ -0,0 +1,5 @@
+package com.hyunki.restapi.events;
+
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
---
+++ src/test/java/com/hyunki/restapi/events/EventTest.java
@@ -0,0 +1,29 @@
+package com.hyunki.restapi.events;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder().build();
+        Assertions.assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean()  {
+        //given
+        String name = "Event";
+        String description = "Spring";
+
+        //when
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        //then
+        Assertions.assertThat(event.getName()).isEqualTo(name);
+        Assertions.assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
