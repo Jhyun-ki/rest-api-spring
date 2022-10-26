7443b3aeef0183bab4c20ddc8ede5a3bdfac6ee2
HyunKi Jung <hjk2334@naver.com>
2022-10-26 15:03:16 +0900

    Event update시 free, offline 여부 테스트 코드 작성

--- src/test/java/com/hyunki/restapi/events/EventTest.java
+++ src/test/java/com/hyunki/restapi/events/EventTest.java
@@ -3,6 +3,8 @@ package com.hyunki.restapi.events;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class EventTest {
 
     @Test
@@ -22,10 +24,71 @@ class EventTest {
         event.setName(name);
         event.setDescription(description);
 
-        System.out.println(event.hashCode());
-
         //then
         Assertions.assertThat(event.getName()).isEqualTo(name);
         Assertions.assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+    @Test
+    public void testFree() {
+        //Given
+        Event event = Event.builder()
+                .basePrice(0)
+                .maxPrice(0)
+                .build();
+
+        //When
+        event.update();
+
+        //Then
+        assertThat(event.isFree()).isTrue();
+
+        //Given
+         event = Event.builder()
+                .basePrice(100)
+                .maxPrice(0)
+                .build();
+
+        //When
+        event.update();
+
+        //Then
+        assertThat(event.isFree()).isFalse();
+
+        //Given
+        event = Event.builder()
+                .basePrice(0)
+                .maxPrice(100)
+                .build();
+
+        //When
+        event.update();
+
+        //Then
+        assertThat(event.isFree()).isFalse();
+    }
+
+    @Test
+    public void testOffline() {
+        //given
+        Event event = Event.builder()
+                .location("강남역 네이버 D2 스타텁 팩토리")
+                .build();
+
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isOffline()).isTrue();
+
+        //given
+        event = Event.builder()
+                .build();
+
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isOffline()).isFalse();
     }
 }
